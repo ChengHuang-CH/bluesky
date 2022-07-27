180d79a2f8d0274a1184bdfcc66b8a1a071714ce
ChengHuang-CH <huangcheng1994@126.com>
2022-07-27 12:08:31 +0100

    bluesky: try to fix triangle

---
+++ bluesky/ui/open3d/osmnx_test.py
@@ -0,0 +1,27 @@
+import osmnx as ox
+import matplotlib.pyplot as plt
+from shapely.ops import triangulate
+
+local_file = '../../../data/osm/cranfield.osm'
+G = ox.graph_from_xml(local_file)
+buildings = ox.geometries_from_xml(local_file, tags={'building': True})
+
+bulding_names = buildings['name'].tolist()
+building_geos = buildings['geometry'].tolist()
+
+index = bulding_names.index('Building 83')
+
+polygon = building_geos[index]
+fig, ax = ox.plot_footprints(buildings)
+
+x, y = polygon.exterior.xy
+# plt.plot(x, y)
+# plt.show()
+
+triangles = triangulate(polygon, tolerance=1.0, edges=False)
+
+for triangle in triangles:
+    plt.plot(*triangle.exterior.xy)
+# print([triangle.wkt for triangle in triangles])
+
+plt.show()
